86871abded8ff3036aaa8be514034c11a2e44af9
Chris Willis <willisc@me.com>
2018-06-13 12:45:23 -0400

    asdf

--- Homework_3/ReactJS_App/shapes/src/index.js
+++ Homework_3/ReactJS_App/shapes/src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import Selectors from './Selectors';
+import TextFields from './TextFields';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<Selectors />, document.getElementById('root'));

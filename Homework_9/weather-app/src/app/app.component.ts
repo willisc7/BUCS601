442cd7b9c4d34e8657534bd2a3da24db6c613c29
Chris Willis <willisc@me.com>
2018-07-22 19:20:54 -0400

    weather app logs http response

--- Homework_9/weather-app/src/app/app.component.ts
+++ Homework_9/weather-app/src/app/app.component.ts
@@ -8,16 +8,13 @@ import { WeatherApiService } from './services/weather-api.service';
 })
 export class AppComponent {
   title = 'app';
-  forecast = '';
 
   constructor(private weatherAPIService: WeatherApiService){ }
 
-/*   getHeroes(): void {
-    this.heroService.getHeroes()
-    .subscribe(heroes => this.heroes = heroes);
-  } */
-
-/*   getWeather(zipcode: string): void{
-    this.weatherAPIService.getWeather(zipcode).subscribe(forecast);
-  } */
+  getWeather(zipcode: string): void {
+    this.weatherAPIService.getWeather(zipcode)
+    .subscribe(response => 
+      console.log(response)
+    )
+  }
 }
--- Homework_9/weather-app/src/app/services/weather-api.service.ts
+++ Homework_9/weather-app/src/app/services/weather-api.service.ts
@@ -1,29 +1,19 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Observable } from '../../../node_modules/rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
+
 export class WeatherApiService {
 
   private weatherURL = 'http://api.openweathermap.org/data/2.5/forecast?';
+  private apiKey = 'babcd9d6ee2c0cd746cc74adca113805';
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
-/*   getHeroes (): Observable<Hero[]> {
-    return this.http.get<Hero[]>(this.heroesUrl)
-      .pipe(
-        tap(heroes => this.log(`fetched heroes`)),
-        catchError(this.handleError('getHeroes', []))
-      );
-  } */
-
-/*   getWeather (zipcode: string): {
-     return this.http.post<Hero>(this.heroesUrl, hero, httpOptions).pipe(
-      tap((hero: Hero) => this.log(`added hero w/ id=${hero.id}`)),
-      catchError(this.handleError<Hero>('addHero'))
-    );
-  } */
+  getWeather(zipcode: string) {
+    return this.http.get(this.weatherURL + 'zip=' + zipcode + ',us&APPID=' + this.apiKey)
+  }
 }
-
-//http://api.openweathermap.org/data/2.5/forecast?zip=ZIP_CODE,us&APPID=babcd9d6ee2c0cd746cc74adca113805

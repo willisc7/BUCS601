61b443718756d550526691ef8587dcf4c2901a6b
Chris Willis <willisc@me.com>
2018-07-22 19:50:31 -0400

    output json

--- Homework_9/weather-app/src/app/app.component.ts
+++ Homework_9/weather-app/src/app/app.component.ts
@@ -8,13 +8,17 @@ import { WeatherApiService } from './services/weather-api.service';
 })
 export class AppComponent {
   title = 'app';
+  json = '';
 
-  constructor(private weatherAPIService: WeatherApiService){ }
+  constructor(private weatherAPIService: WeatherApiService) { }
 
   getWeather(zipcode: string): void {
+
+    // store response in json variable
     this.weatherAPIService.getWeather(zipcode)
-    .subscribe(response => 
-      console.log(response)
-    )
+      .subscribe(response => {
+        this.json = JSON.stringify(response);
+        console.log(response);
+      })
   }
 }

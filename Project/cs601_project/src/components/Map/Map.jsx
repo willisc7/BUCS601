ce0bd9acbe6e6c7be5a3cd717f9ca98f8b9fcd54
Chris Willis <willisc@me.com>
2018-07-28 12:04:30 -0400

    markers showing pictures when clicked!!!

--- Project/cs601_project/src/components/Map/Map.jsx
+++ Project/cs601_project/src/components/Map/Map.jsx
@@ -148,67 +148,50 @@ class Map extends React.Component {
             </GoogleMap>
         );
 
-        /*         const RestaurantMarker = compose(
-                    withStateHandlers(() => ({
-                        isOpen: false,
-                    }), {
-                            onToggleOpen: ({ isOpen }) => () => ({
-                                isOpen: !isOpen,
-                            })
-                        }),
-                    withProps(name, latitude, longitude)
-                )(props =>
-                    <Marker key={name} position={{ lat: latitude, lng: longitude }}>
-                        {props.isOpen && <InfoWindow onCloseClick={props.onToggleOpen}>
-                            <img src="https://pbs.twimg.com/media/DYGA5cFUMAc1zfY.jpg" alt="" />
-                        </InfoWindow>}
-                    </Marker>
-                ); */
-
         return (
             <MapWithASearchBox />
         );
     }
 }
 
-/* class RestaurantMarker extends React.Component {
-
-    state = {
-        isOpen: false
-    }
-
-    onToggleOpen = ({ isOpen }) => () => ({
-        isOpen: !isOpen,
-    })
-
-    render() {
-
-        const { id, name, latitude, longitude } = this.props
-
-        return (
-            <Marker key={id} name={name} position={{ lat: latitude, lng: longitude }}>
-                {this.state.isOpen && <InfoWindow onCloseClick={this.state.onToggleOpen}>
-                    <img src="https://pbs.twimg.com/media/DYGA5cFUMAc1zfY.jpg" alt="" />
-                </InfoWindow>}
-            </Marker>
-        )
-    }
-} */
-
 class RestaurantMarker extends React.Component {
 
-    state = { open: false }
+    state = {
+        isOpen: [],
+        onToggleOpen: (marker_id) => {
+            let isOpen = [...this.state.isOpen]
+
+            // if array is empty, add entry for the marker
+            if (isOpen.length === 0) {
+                isOpen.push([marker_id], [false])
+                this.setState({ isOpen })
+                console.log("isOpen=" + this.state.isOpen)
+                return
+            }
+
+            // find the array with marker_id in it and flip its state
+            isOpen[1] = !isOpen[1]
+
+            // update isOpen state
+            this.setState({ isOpen })
+            console.log("isOpen[1]=" + this.state.isOpen[1])
+        }
+    }
 
     render() {
-
         const { id, name, latitude, longitude } = this.props
 
-        // Return Restaurant Marker Component.
         return (
-            <Marker key={id} position={{ lat: latitude, lng: longitude }}>
-                {this.state.open && (
-                    <InfoWindow onClick={() => this.setState(state => ({ open: !state.open }))}> {name} </InfoWindow>
-                )}
+            <Marker key={id}
+                name={name}
+                position={{ lat: latitude, lng: longitude }}
+                onClick={() => { this.state.onToggleOpen(id) }}
+            >
+                {this.state.isOpen[1] && <InfoWindow id={id}
+                    onCloseClick={() => { this.state.onToggleOpen(id) }}
+                >
+                    <img src="https://pbs.twimg.com/media/DYGA5cFUMAc1zfY.jpg" alt="" />
+                </InfoWindow>}
             </Marker>
         )
 

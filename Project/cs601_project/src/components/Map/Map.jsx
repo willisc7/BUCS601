ef319f3892132a50a949816eb4261e928ec64e67
Chris Willis <willisc@me.com>
2018-07-28 12:12:46 -0400

    made RestaurantMarker its own component

--- Project/cs601_project/src/components/Map/Map.jsx
+++ Project/cs601_project/src/components/Map/Map.jsx
@@ -10,6 +10,7 @@ import Button from "components/CustomButtons/Button.jsx";
 import { withScriptjs, withGoogleMap, GoogleMap, Marker, InfoWindow } from "react-google-maps";
 import SearchBox from "react-google-maps/lib/components/places/SearchBox";
 import { compose, withProps, lifecycle, withStateHandlers } from "recompose";
+import RestaurantMarker from './RestaurantMarker.jsx'
 
 /*global google*/
 
@@ -152,51 +153,6 @@ class Map extends React.Component {
             <MapWithASearchBox />
         );
     }
-}
-
-class RestaurantMarker extends React.Component {
-
-    state = {
-        isOpen: [],
-        onToggleOpen: (marker_id) => {
-            let isOpen = [...this.state.isOpen]
-
-            // if array is empty, add entry for the marker
-            if (isOpen.length === 0) {
-                isOpen.push([marker_id], [false])
-                this.setState({ isOpen })
-                console.log("isOpen=" + this.state.isOpen)
-                return
-            }
-
-            // find the array with marker_id in it and flip its state
-            isOpen[1] = !isOpen[1]
-
-            // update isOpen state
-            this.setState({ isOpen })
-            console.log("isOpen[1]=" + this.state.isOpen[1])
-        }
-    }
-
-    render() {
-        const { id, name, latitude, longitude } = this.props
-
-        return (
-            <Marker key={id}
-                name={name}
-                position={{ lat: latitude, lng: longitude }}
-                onClick={() => { this.state.onToggleOpen(id) }}
-            >
-                {this.state.isOpen[1] && <InfoWindow id={id}
-                    onCloseClick={() => { this.state.onToggleOpen(id) }}
-                >
-                    <img src="https://pbs.twimg.com/media/DYGA5cFUMAc1zfY.jpg" alt="" />
-                </InfoWindow>}
-            </Marker>
-        )
-
-    }
-
 }
 
 export default withStyles(productStyle)(Map);
